8ba2a5f33a64a6d14699ecb7f7af963bacf4cef8
MadhuriMaturi <151865379+MadhuriMaturi@users.noreply.github.com>
2023-12-09 20:25:03 +0530

    week-4[2]

---
+++ week-4[2]/Spring-secutity-bcypt/src/main/java/com/hexaware/springsecuritybcrypt/SpringSecutityBcyptApplication.java
@@ -0,0 +1,13 @@
+package com.hexaware.springsecuritybcrypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecutityBcyptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecutityBcyptApplication.class, args);
+	}
+
+}
---
+++ week-4[2]/Spring-secutity-bcypt/src/test/java/com/hexaware/springsecuritybcrypt/SpringSecutityBcyptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexaware.springsecuritybcrypt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecutityBcyptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
